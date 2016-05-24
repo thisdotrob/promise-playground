e233f2c0790954597c00ba194ea5589025e98b8b
Rob Stevenson <robstevenson000@gmail.com>
2016-05-24 20:52:47 +0100

    Update eslintrc

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 'use strict';
 
+const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const app = express();
+
+MongoClient.connect('mongodb://localhost:27017/books', (err, db) => {
+  if (err) {
+    console.log('Error connecting to MongoDB:', err);
+  } else {
+    console.log('Succesfully connected to MongoDB');
+    db.close();
+  }
+
+});
 
 app.get('/', (req, res) => {
   let date = new Date();
@@ -9,5 +20,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('listening on port 3000');
+  console.log('Listening on port 3000');
 });
