4b0a88116ed0c05a3e19652efe8c88f172d09010
Rob Stevenson <robstevenson000@gmail.com>
2016-05-25 17:29:00 +0100

    Store book in db

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 'use strict';
 
 const MongoClient = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
 const express = require('express');
 const app = express();
+const json = require('body-parser').json;
+const dbUrl = 'mongodb://localhost:27017/books';
 
-MongoClient.connect('mongodb://localhost:27017/books', (err, db) => {
-  if (err) throw Error(err);
-  console.log('Succesfully connected to MongoDB');
-  insertDocument(db, () => db.close());
+app.post('/example2', json(), (req, res) => {
+  MongoClient.connect(dbUrl)
+    .then((db) => insertBook(db, req.body))
+    .then((result) => res.status(200).send(result))
+    .catch((err) => res.status(503).send('Error: ' + err.message));
 });
 
-app.get('/', (req, res) => {
-  let date = new Date();
-  res.status(200).json({ timestamp: date.toISOString() });
-});
-
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
-});
-
-
-function insertDocument(db, callback) {
-  db.collection('books').insertOne( {
-    
+function insertBook(db, json) {
+  return new Promise((resolve, reject) => {
+    db.collection('books').insertOne(json)
+      .then((result) => {
+        db.close();
+        resolve(result);
+      })
+      .catch((err) => reject(err));
   });
-  callback();
 }
+
+app.listen(3000, () => console.log('Listening on port 3000'));
