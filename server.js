78fff76d8dc16e2dee47ae2102b4390ba34dde50
Rob Stevenson <robstevenson000@gmail.com>
2016-07-01 10:33:27 +0100

    Add .catch nesting example

--- server.js
+++ server.js
@@ -6,6 +6,61 @@ const app = express();
 const json = require('body-parser').json;
 const dbUrl = 'mongodb://localhost:27017/books';
 
+
+// This example needs a corresponding .catch for each .then because they are nested...
+app.get('/bad-promise-example/:stringToTransform', (req, res) => {
+  removeWhiteSpace(req.params.stringToTransform)
+    .then(stringWithNoWhiteSpace => {
+      removeNonNumeric(stringWithNoWhiteSpace)
+        .then(numericString => {
+          multiplyByTen(numericString)
+            .then(integer => {
+              res.status(200).send(integer);
+            })
+            .catch(err => res.status(500).send(err.message));
+        })
+        .catch(err => res.status(500).send(err.message));
+    })
+    .catch(err => res.status(500).send(err.message));
+});
+
+// This example only needs one .catch, but is limited to a single argument for each chained promise returning function
+app.get('/good-promise-example/:stringToTransform', (req, res) => {
+  removeWhiteSpace(req.params.stringToTransform)
+    .then(removeNonNumeric)
+    .then(multiplyByTen)
+    .catch(err => res.status(500).send(err.message));
+});
+
+
+
+
+function removeWhiteSpace(string) {
+  return new Promise(resolve => {
+    // throw Error('Error thrown in removeWhiteSpace');
+    resolve(string.replace(/\s/g, ''));
+  });
+}
+
+// If we wanted this to take more than one argument, we could use an object instead
+function removeNonNumeric(string) {
+  return new Promise(resolve => {
+    // throw Error('Error thrown in removeNonNumeric');
+    resolve(string.replace(/\D/g,''));
+  });
+}
+
+function multiplyByTen(string) {
+  return new Promise(resolve => {
+    throw Error('Error thrown in multiplyByTen');
+    let integer = Math.floor(string) * 10;
+    resolve(integer.toString());
+  });
+}
+
+
+
+
 app.post('/book', json(), (req, res) => {
   MongoClient.connect(dbUrl)
     .then((db) => insertBook(db, req.body))
